9b0729ec713d44faedfb46246b9d3addce1a2405
Puleng <mtambo.ap@gmail.com>
2024-05-19 17:36:03 +0200

    Added Login.feature code

--- src/test/java/steps/StepDefinition.java
+++ src/test/java/steps/StepDefinition.java
@@ -1,4 +1,29 @@
 package steps;
 
-public class StepDefinition {
+import io.cucumber.java.en.*;
+
+public class StepDefinition extends Base{
+        @Given("I am on Login page")
+    public void i_am_on_login_page(){
+    }
+
+    @And("^I enter the username (.*)$")
+    public void i_enter_the_username_user_name(String username) {
+    }
+    @And("^I enter the password (.*)$")
+    public void i_enter_th_password_pass(String password) {
+    }
+
+    @When("I click on login button")
+    public void i_click_on_login_button() {
+    }
+
+    @Then("The homepage is displayed")
+    public void the_homepage_is_displayed() {
+    }
+
+//    @Then("I quit the browser")
+//    public void iQuitTheBrowser() {
+//        driver.quit();
+//    }
 }
