44dbfaaf9aad1293c130691a861a08993a4f967e
Puleng <mtambo.ap@gmail.com>
2024-05-19 17:14:54 +0200

    Added Base class for mappings

---
+++ src/test/java/steps/Base.java
@@ -0,0 +1,14 @@
+package steps;
+
+import Helpers.BrowserFactory;
+import Pages.HomePage;
+import Pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Base {
+    BrowserFactory browserFactory = new BrowserFactory();
+    final WebDriver driver = browserFactory.startBrowser("chrome","url");
+    LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+}
